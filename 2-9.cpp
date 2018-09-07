6a32fe8d793835272859ea94adda0a3d85664cf7
Igarashi, Masanori <masanori.igarashi1@gmail.com>
2018-09-07 10:00:28 +0900

    add 2-9, thread return value

--- 2-9.cpp
+++ 2-9.cpp
@@ -5,7 +5,7 @@
 #include "helper.h"
 #include <cstdint>
 
-/* Sample for thread id and thread sync */
+/* Sample for thread join and return */
 
 namespace {
 
@@ -13,29 +13,32 @@ void *threadFunc(void *arg)
 {
 	int n = static_cast<int>(reinterpret_cast<int64_t>(arg));
 
-	for (int i = 0; i < n; ++i)
+	for (int i = 0; i < n; i++)
 	{
 		VPRINTF("I'm threadFunc: %d\n", i);
 		sleep(1);
 	}
 
-	return nullptr;
+	return reinterpret_cast<void*>(n);
 }
 
 } // anonymouse namespace
 
 
-int p2_6_first_thread3(int argc, char *argv[])
+int p2_9_first_thread4(int argc, char *argv[])
 {
     VPRINTF("Hello world\n");
 
 	int n;
+
 	if (argc > 1)
 		n = atoi(argv[1]);
 	else
-		n = 8;
+		n = 3;
+
 
 	pthread_t thread;
+
 	if (pthread_create(&thread, NULL, threadFunc, reinterpret_cast<void*>(n)) != 0)
 	{
 		VPRINTF("error: failed to create new thread.\n");
@@ -44,16 +47,19 @@ int p2_6_first_thread3(int argc, char *argv[])
 
 	for (int i = 0; i < 5; i++)
 	{
-		VPRINTF("I'm main: %d\n", i);;
+		VPRINTF("I'm main: %d\n", i);
 		sleep(1);
 	}
 
-	if (pthread_join(thread, NULL) != 0)
+	int ret;
+
+	if (pthread_join(thread, (void**)&ret) != 0)
 	{
 		VPRINTF("error: failed to wait for the thread termination.\n");
 		exit(1);
 	}
 
+	VPRINTF("threadFunc has been terminated with number %d\n", ret);
 	VPRINTF("Bye.\n");
 
     return 0;
--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 int p2_1_first_thread(int argc, char *argv[]);
 int p2_3_first_thread2(int argc, char *argv[]);
 int p2_6_first_thread3(int argc, char *argv[]);
+int p2_9_first_thread4(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
     //p2_1_first_thread(argc, argv);
     //p2_3_first_thread2(argc, argv);
-	p2_6_first_thread3(argc, argv);
+	//p2_6_first_thread3(argc, argv);
+	p2_9_first_thread4(argc, argv);
 
 	return 0;
 }
