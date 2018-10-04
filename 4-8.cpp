b95cb7faea1055a21cc378567e541adbb350e525
Igarashi, Masanori <masanori.igarashi1@gmail.com>
2018-10-04 10:08:31 +0900

    add 4-8, fly

---
+++ 4-8.cpp
@@ -0,0 +1,230 @@
+#include <cerrno>
+#include <cstdio>
+#include <cstdlib>
+#include <cstdint>
+#include <pthread.h>
+#include <cmath>
+#include "helper.h"
+
+namespace
+{
+    typedef struct
+    {
+        char    mark;
+        double  x;
+        double  y;
+        double  angle;
+        double  speed;
+    } Fly;
+
+    const int32_t kWidth = 78;
+    const int32_t kHeight = 23;
+    const int32_t kMaxFly = 6;
+    const char * const kFlyMarkList = "o@*+.#";
+    const int32_t kDrawCycle = 50;
+    const float kMinSpeed = 1.0f;
+    const float kMaxSpeed = 20.0f;
+
+    bool g_stop_request;
+    pthread_mutex_t g_mutex;
+
+    Fly g_fly_list[kMaxFly];
+
+    void mSleep(int32_t msec)
+    {
+        struct timespec ts;
+        ts.tv_sec = msec/1000;
+        ts.tv_nsec = (msec % 1000) * 1000 * 1000;
+        nanosleep(&ts, NULL);
+    }
+
+    double randDouble(double min_value, double max_value)
+    {
+        return min_value + (double)rand()/((double)RAND_MAX + 1) * (max_value - min_value);
+    }
+
+    void clearScreen()
+    {
+        //fputs("\033[2J", stdout); // this escape code clears screen; this does not work in C++
+		std::system("clear");
+    }
+
+    void moveCursor(int32_t x, int32_t y)
+    {
+        //printf("\033[%d;%dH", y, x); // this does not work in C++
+    }
+
+    void flyInitRandom(Fly *fly, char mark_)
+    {
+        fly->mark = mark_;
+        fly->x = randDouble(0, static_cast<double>(kWidth - 1));
+        fly->y = randDouble(0, static_cast<double>(kHeight - 1));
+        fly->angle = randDouble(0, M_2_PI);
+        fly->speed = randDouble(kMinSpeed, kMaxSpeed);
+    }
+
+    void flyMove(Fly *fly)
+    {
+		fly->x += cos(fly->angle);
+		fly->y += sin(fly->angle);
+
+		/* x方向の縁にぶつかったら方向を変える */
+		if (fly->x < 0)
+		{
+			fly->x = 0;
+			fly->angle = M_PI - fly->angle;
+		}
+		else if (fly->x > kWidth - 1)
+		{
+			fly->x = kWidth - 1;
+			fly->angle = M_PI - fly->angle;
+		}
+
+		/* y方向の縁にぶつかったら方向を変える */
+		if (fly->y < 0)
+		{
+			fly->y = 0;
+			fly->angle = -(fly->angle);
+		}
+		else if (fly->y > kHeight - 1)
+		{
+			fly->y = kHeight - 1;
+			fly->angle = -(fly->angle);
+		}
+    }
+
+    bool isFlyAt(const Fly &fly, int32_t x, int32_t y)
+    {
+		return (static_cast<int>(fly.x) == x) && (static_cast<int>(fly.y) == y);
+    }
+
+    void *doMove(void *arg)
+    {
+        Fly *fly = (Fly*)arg;
+
+        while (!g_stop_request)
+        {
+            pthread_mutex_lock(&g_mutex);
+			{
+				flyMove(fly);
+			}
+            pthread_mutex_unlock(&g_mutex);
+
+            mSleep(static_cast<int>(1000.0 / fly->speed));
+        }
+
+        return nullptr;
+    }
+
+
+    void drawScreen()
+    {
+        moveCursor(0, 0);
+
+		for (int32_t y = 0; y < kHeight; ++y)
+		{
+			for (int32_t x = 0; x < kWidth; ++x)
+			{
+				char ch = 0;
+
+				/* if thre is a fly at (x, y), show its mark */
+				for (int32_t i = 0; i < kMaxFly; ++i)
+				{
+					if (isFlyAt(g_fly_list[i], x, y))
+					{
+						ch = g_fly_list[i].mark;
+						break;
+					}
+				}
+
+				if (ch != 0)
+				{
+					putchar(ch);
+				}
+				else if ((y == 0) || (y == kHeight - 1))
+				{
+					/* print upper/lower edge */
+					putchar('-');
+				}
+				else if ((x == 0) || (x == kWidth - 1))
+				{
+					/* print right/left edge */
+					putchar('|');
+				}
+				else
+				{
+					putchar(' ');
+				}
+			}
+
+			putchar('\n');
+		}
+    }
+
+
+    void *doDraw(void *arg)
+    {
+		while (!g_stop_request)
+		{
+			pthread_mutex_lock(&g_mutex);
+			{
+				drawScreen();
+			}
+			pthread_mutex_unlock(&g_mutex);
+
+			mSleep(kDrawCycle);
+		}
+
+        return nullptr;
+    }
+
+} // anonymouse namespace
+
+
+int p4_8_fly(int argc, char *argv[])
+{
+	g_stop_request = false;
+
+    srand(static_cast<unsigned int>(time(NULL)));
+
+    pthread_mutex_init(&g_mutex, NULL);
+
+    clearScreen();
+
+    for (int32_t i = 0; i < kMaxFly; ++i)
+    {
+        flyInitRandom(&g_fly_list[i], kFlyMarkList[i]);
+    }
+
+
+	/* Fly move */
+    pthread_t move_thread[kMaxFly];
+
+    for (int32_t i = 0; i < kMaxFly; ++i)
+    {
+        pthread_create(&move_thread[i], NULL, doMove, reinterpret_cast<void*>(&g_fly_list[i]));
+    }
+
+
+	/* Rendering */
+	pthread_t draw_thread;
+	pthread_create(&draw_thread, NULL, doDraw, NULL);
+
+
+	/* メインスレッドは何か入力されるのを待つだけ */
+	char buf[40];
+	fgets(buf, sizeof(buf), stdin);
+	g_stop_request = true;
+
+
+	pthread_join(draw_thread, NULL);
+	for (int32_t i = 0; i < kMaxFly; ++i)
+	{
+		pthread_join(move_thread[i], NULL);
+	}
+
+
+    pthread_mutex_destroy(&g_mutex);
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ int p3_10_maxData2(int argc, char *argv[]);
 int p4_1_primeNumber(int argc, char *argv[]);
 int p4_4_primeNumber5(int argc, char *argv[]);
 int p4_6_fastPrimeNumber(int argc, char *argv[]);
+int p4_8_fly(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
@@ -32,7 +33,8 @@ int main(int argc, char *argv[])
     //p3_10_maxData2(argc, argv);
     //p4_1_primeNumber(argc, argv);
     //p4_4_primeNumber5(argc, argv);
-    p4_6_fastPrimeNumber(argc, argv);
+    //p4_6_fastPrimeNumber(argc, argv);
+    p4_8_fly(argc, argv);
 
 	return 0;
 }
