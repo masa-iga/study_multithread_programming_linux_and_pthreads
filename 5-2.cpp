61fe05a128296bb90cdb6bf1449bf2233690fe5d
Igarashi, Masanori <masanori.igarashi1@gmail.com>
2018-10-16 10:01:06 +0900

    update 5-2; introduce cond_wait_timedwait

--- 5-2.cpp
+++ 5-2.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <pthread.h>
 #include <unistd.h>
+#include <time.h>
+#include <errno.h>
 #include "helper.h"
 
 pthread_mutex_t g_mutex;
@@ -19,13 +21,31 @@ namespace {
         {
             VPRINTF("wait for signal.\n");
 
+#if 0
             if (pthread_cond_wait(&g_cond, &g_mutex) != 0)
             {
                 VPRINTF("error on pthread_cond_wait\n");
                 exit(1);
             }
-
             VPRINTF("got a signal\n");
+#else
+			struct timespec ts;
+			clock_gettime(CLOCK_REALTIME, &ts);
+			ts.tv_sec += 2;
+
+			switch (pthread_cond_timedwait(&g_cond, &g_mutex, &ts))
+			{
+			case 0:
+				VPRINTF("got a signal.\n");
+				break;
+			case ETIMEDOUT:
+				VPRINTF("timeout.\n");
+				break;
+			default:
+				VPRINTF("error on pthread_cond_wait\n");
+				exit(1);
+			}
+#endif
         }
         pthread_mutex_unlock(&g_mutex);
 
@@ -43,7 +63,7 @@ int p5_2_condWait(int argc, char *argv[])
     pthread_t thread;
     pthread_create(&thread, NULL, threadFunc, NULL);
 
-    sleep(3);
+    sleep(1);
 
     VPRINTF("send signal\n");
     pthread_cond_signal(&g_cond);
